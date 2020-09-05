2cca090e8faddf1f32da1303772afb353f5584b3
Luis Chavez <darkaw_remse@yahoo.com>
2020-09-05 16:29:03 -0500

    Add view to show products

---
+++ Gapsi/ProductsCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductsCell.swift
+//  Gapsi
+//
+//  Created by Darkaw on 05/09/20.
+//  Copyright © 2020 dr. All rights reserved.
+//
+
+import UIKit
+
+class ProductsCell: UITableViewCell {
+    @IBOutlet weak var labelTitle: UILabel!
+   @IBOutlet weak var labelPrice: UILabel!
+    
+}
